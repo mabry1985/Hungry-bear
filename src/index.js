b274dc15f7304bcf43228d2c410669327da805d9
bcauchon <bcauchon@gmail.com>
2019-06-25 23:59:55 -0700

    add images using file loader and html loader

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+let clickCount = 0;
 
 $(document).ready(function() {
   $('button').click(function () {
-    $('body').append(`<p>blue button clicked</p>`);
-
+    clickCount++;
+    $('#click-count').text(clickCount);
   });
 });
