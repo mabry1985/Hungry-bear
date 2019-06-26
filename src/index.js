07f6af11cdc032a8fd5a1a5fe62f388baf7f765c
bcauchon <bcauchon@gmail.com>
2019-06-25 22:20:51 -0700

    move styles.css to css folder and make background green

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import './css/styles.css';
 
 $(document).ready(function() {
   console.log("document ready");
